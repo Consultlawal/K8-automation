4da8fddd5548c3aa4fc4ee0adf9683dca0939f04
Consultlawal <101852337+Consultlawal@users.noreply.github.com>
2023-01-10 01:04:13 -0600

    Update route53.tf

--- 14-NOV-Sock-Shop-microservices-Kubernetes-Project---US-team-1/route53.tf
+++ 14-NOV-Sock-Shop-microservices-Kubernetes-Project---US-team-1/route53.tf
@@ -1,15 +1,15 @@
-resource "aws_route53_zone" "routezone" {
-  name          = var.domain_name
-  force_destroy = true
-}
-# Route 53 A Record
-resource "aws_route53_record" "route53_record" {
-  zone_id = aws_route53_zone.routezone.zone_id
-  name    = var.domain_name
-  type    = "A"
-  alias {
-    name                   = aws_lb.k8s-alb.dns_name
-    zone_id                = aws_lb.k8s-alb.zone_id
-    evaluate_target_health = false
-  }
-}
+# resource "aws_route53_zone" "routezone" {
+#   name          = var.domain_name
+#   force_destroy = true
+# }
+# # Route 53 A Record
+# resource "aws_route53_record" "route53_record" {
+#   zone_id = aws_route53_zone.routezone.zone_id
+#   name    = var.domain_name
+#   type    = "A"
+#   alias {
+#     name                   = aws_lb.k8s-alb.dns_name
+#     zone_id                = aws_lb.k8s-alb.zone_id
+#     evaluate_target_health = false
+#   }
+# }
